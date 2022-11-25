a333bbc6a6b2f16bb573dfc87a10b0c377cbede8
regemtenebris <82021629+regemtenebris@users.noreply.github.com>
2022-11-25 15:40:57 +0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,9 @@ import {
 
 import Login from "./components/loginpage";
 import Main from "./components/mainpage";
-
+import Admin from "./components/adminpage";
+import RegDoctor from "./components/registrationdoctor";
+import RegPatient from "./components/registrationpatient";
   
 function App() {
   return (
@@ -18,6 +20,9 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Main />} />
           <Route path="/loginpage" element={<Login />} />   
+          <Route path="/adminpage" element={<Admin />} />   
+          <Route path="/registrationdoctor" element={<RegDoctor />} />   
+          <Route path="/registrationpatient" element={<RegPatient />} />   
         </Routes>
       </Router>
   );
